4912f2d2764563c62f8865296709e0e3a8a819f6
Elton Ramaldes <eltonramaldes@MacBook-Pro-de-Elton.local>
2025-11-12 21:14:34 -0300

    Fix import useBCTPrice

--- app/inicio/page.tsx
+++ app/inicio/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { useState, useEffect } from "react";
-import useBCTPrice from "@/hooks/useBCTPrice";
+import { useBCTPrice } from "@/hooks/useBCTPrice"; // ✔️ CORRETO
 
 // Função para buscar o dólar comercial
 async function fetchUSD() {
